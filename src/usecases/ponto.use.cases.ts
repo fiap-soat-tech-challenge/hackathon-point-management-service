1e5513c31f8c96b9e4566206d386e8c78f4c59f4
Jonilson Sousa <jonilsonds9@gmail.com>
2024-03-19 09:50:12 -0300

    Adicionando estrutura de pastas

---
+++ src/usecases/ponto.use.cases.ts
@@ -0,0 +1,8 @@
+import { Ponto } from '../domain/model/ponto';
+
+export class PontoUseCases {
+  addPonto(ponto: Ponto) {
+    console.log(ponto);
+    throw new Error('Method not implemented.');
+  }
+}
